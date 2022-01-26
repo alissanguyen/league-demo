070cf53311cc788fe72669e87a8f704120a93a83
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-01-25 16:54:22 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
